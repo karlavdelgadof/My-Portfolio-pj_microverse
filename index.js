f78fad23380addeade2c31674c431b48da627cae
karlavdelgadof <karlavdelgadof@gmail.com>
2022-06-13 18:44:02 -0500

    Add active class for hmg-icon/close

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ const hmgIcon = document.querySelector(".hmg-menu");
 const overlayMenu = document.querySelector(".overlay-menu");
 const closeBtn = document.querySelector(".close");
 
+hmgIcon.addEventListener("click", () => {
+  hmgIcon.classList.toggle("active");
+  overlayMenu.classList.toggle("active");
+})
+
+closeBtn.addEventListener("click", () => {
+  hmgIcon.classList.toggle("active");
+  overlayMenu.classList.toggle("active");
+  }) 
+
+
+document.querySelectorAll(".link").forEach(n => n.
+  addEventListener("click", () => {
+    hmgIcon.classList.remove("active");
+    overlayMenu.classList.remove("active");
+  }))
+
+document.querySelectorAll(".close").forEach(n => n.
+  addEventListener("click", () => {
+    hmgIcon.classList.remove("active");
+    overlayMenu.classList.remove("active");
+  }))
